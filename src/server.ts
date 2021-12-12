89590f673412ade6b2efe99c329e7057e633859f
Pedro Carvalho <eng.pedro.amb@gmail.com>
2021-12-12 16:08:57 -0300

    feat: eslint and prettier

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
-import express from 'express';
-import { createCourse } from './routes';
+import express from "express";
 
 const app = express();
 
-app.get('/', createCourse);
+app.get("/", (req, res) => {
+  res.json({ message: "Hello World!!!" });
+});
 
-app.listen(3333);
+app.listen(3333, () => {
+  console.log("Server is running!!");
+});
