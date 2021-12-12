941cfc5e1417c63b7665f20429e7f4e1e3ba680b
Pedro Carvalho <eng.pedro.amb@gmail.com>
2021-12-12 16:37:11 -0300

    feat: set debbuger

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,16 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.json({ message: "Hello World!!!" });
+});
+
+app.post("/courses", (req, res) => {
+  const { name } = req.body;
+
+  return res.json({ name });
 });
 
 app.listen(3333, () => {
