22dc383ab8018cae456a698ecc418cb7e8af85e5
Pedro Carvalho <eng.pedro.amb@gmail.com>
2021-12-13 14:05:28 -0300

    feat: type categories route

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,20 +1,25 @@
 import { Router } from "express";
 import { v4 as uuidV4 } from "uuid";
 
+import { Category } from "../model/Category";
+
 const categoriesRoutes = Router();
 
-const categories = [];
+const categories: Category[] = [];
 
 categoriesRoutes.post("/", (req, res) => {
   const { name, description } = req.body;
 
-  const category = {
+  const category = new Category();
+
+  Object.assign(category, {
     name,
     description,
-    id: uuidV4(),
-  };
+    created_at: new Date(),
+  });
 
   categories.push(category);
+  console.log(category);
 
   return res.status(201).send();
 });
