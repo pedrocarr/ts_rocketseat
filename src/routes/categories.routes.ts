3f790aa7670e7fc83b232a4c539dcb0c6562e65d
Pedro Carvalho <eng.pedro.amb@gmail.com>
2021-12-28 16:49:35 -0300

    feat: createCategoryController

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,13 +1,15 @@
 import { Router } from "express";
 
 import { CategoriesRepository } from "../modules/cars/repositories/CategoriesRepository";
-import { CreateCategoryService } from "../modules/cars/services/CreateCategoryService";
+import { createCategoryController } from "../modules/cars/useCases/createCategory";
 
 const categoriesRoutes = Router();
 
 const categoriesRepository = new CategoriesRepository();
 
-categoriesRoutes.post("/", (req, res) => {});
+categoriesRoutes.post("/", (req, res) => {
+  return createCategoryController.handle(req, res);
+});
 
 categoriesRoutes.get("/", (req, res) => {
   const all = categoriesRepository.list();
