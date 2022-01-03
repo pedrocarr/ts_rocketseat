22a59fcf287297d981696887688343ad3e512cbd
Pedro Carvalho <eng.pedro.amb@gmail.com>
2022-01-03 09:58:45 -0300

    feat: add multer dependencies

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
+import multer from "multer";
 
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
 import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 
 const categoriesRoutes = Router();
+
+const upload = multer({});
 
 categoriesRoutes.post("/", (req, res) => {
   return createCategoryController.handle(req, res);
@@ -12,5 +15,7 @@ categoriesRoutes.post("/", (req, res) => {
 categoriesRoutes.get("/", (req, res) => {
   return listCategoriesController.handle(req, res);
 });
+
+categoriesRoutes.post("/import", (req, res) => {});
 
 export { categoriesRoutes };
