d0339185ef0324e2b4d8882a30073b5320bf3755
Pedro Carvalho <eng.pedro.amb@gmail.com>
2022-01-03 12:34:19 -0300

    feat: readStream

--- src/modules/cars/useCases/importCategory/importCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/importCategoryUseCase.ts
@@ -1,6 +1,18 @@
+// eslint-disable-next-line import/no-unresolved
+import { parse as csvParse } from "csv-parse";
+import fs from "fs";
+
 class ImportCategoryUseCase {
   execute(file: Express.Multer.File): void {
-    console.log(file);
+    const stream = fs.createReadStream(file.path);
+
+    const parseFile = csvParse();
+
+    stream.pipe(parseFile);
+
+    parseFile.on("data", async (line) => {
+      console.log(line);
+    });
   }
 }
 
