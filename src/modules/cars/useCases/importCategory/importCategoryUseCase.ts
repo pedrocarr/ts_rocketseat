c3059c2b495a406ecf8b8803f42c5d1f6d940f01
Pedro Carvalho <eng.pedro.amb@gmail.com>
2022-01-04 20:38:13 -0300

    chore: add categories repository to import category use case

--- src/modules/cars/useCases/importCategory/importCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/importCategoryUseCase.ts
@@ -2,7 +2,10 @@
 import { parse as csvParse } from "csv-parse";
 import fs from "fs";
 
+import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
+
 class ImportCategoryUseCase {
+  constructor(private categoriesRepository: ICategoriesRepository) {}
   execute(file: Express.Multer.File): void {
     const stream = fs.createReadStream(file.path);
 
