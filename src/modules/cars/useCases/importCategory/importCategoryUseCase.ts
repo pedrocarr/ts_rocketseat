cf59708be16029b3390bad06bb71813c7d35f0f5
Pedro Carvalho <eng.pedro.amb@gmail.com>
2022-01-10 09:43:05 -0300

    feat: finsih Promise Import Category Use Case

--- src/modules/cars/useCases/importCategory/importCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/importCategoryUseCase.ts
@@ -39,7 +39,7 @@ class ImportCategoryUseCase {
     });
   }
 
-  async execute(file: Express.Multer.File): void {
+  async execute(file: Express.Multer.File): Promise<void> {
     const categories = await this.loadCategories(file);
     console.log(categories);
   }
